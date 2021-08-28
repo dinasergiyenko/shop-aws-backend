617adefa94840102dba146976c8da5ec5fdbd5dd
dinasergiyenko <sergidka@gmail.com>
2021-08-29 00:20:47 +0300

    Create unit tests

---
+++ ProductServiceTests/GetProductListHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Moq;
+using ProductService.Handlers;
+using Xunit;
+
+namespace ProductServiceTests
+{
+    public class GetProductsListHandlerTests
+    {
+        [Fact]
+        public async Task GetAllProducts_ReadProducts_ReturnsProducts()
+        {
+            var handler = new GetProductListHandler();
+
+            var response = await handler.GetProductListAsync(It.IsAny<APIGatewayProxyRequest>(), It.IsAny<ILambdaContext>());
+
+            Assert.NotEmpty(response.Body);
+        }
+    }
+}
