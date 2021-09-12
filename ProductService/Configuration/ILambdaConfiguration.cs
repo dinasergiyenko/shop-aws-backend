a501137489cb260737593d74ce15a5fbd7e63d33
dinasergiyenko <sergidka@gmail.com>
2021-09-12 13:23:11 +0300

    Add calls to db to retrieve products and product by id

---
+++ ProductService/Configuration/ILambdaConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ProductService.Configuration
+{
+    public interface ILambdaConfiguration
+    {
+        public IConfiguration Configuration { get; }
+    }
+}
